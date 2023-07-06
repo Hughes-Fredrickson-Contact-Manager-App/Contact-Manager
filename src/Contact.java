be4192be2473bb4a5917e58989b7d4c3f11e4d4b
Stephen Fredrickson <stephen.fredrickson@gmail.com>
2023-07-06 06:47:07 -0500

    refactored code

--- src/Contact.java
+++ src/Contact.java
@@ -1,4 +1,109 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Contact {
+	Scanner scanner = new Scanner(System.in);
     String name;
     String phoneNumber;
+    
+   public void menu() {
+    	 System.out.println("1. View contacts.");
+         System.out.println("2. Add a new contact.");
+         System.out.println("3. Search a contact by name.");
+         System.out.println("4. Delete an existing contact.");
+         System.out.println("5. Exit.");
+         System.out.println("Enter an option (1-5) then press Enter:");
+         
+
+    }
+   
+   public void showContacts() throws IOException {
+	   Path contactPath = Paths.get("data", "Contacts.txt");
+       List<String> contactListFromFile = Files.readAllLines(contactPath);
+       for (int i = 0; i < contactListFromFile.size(); i += 1) {
+
+           System.out.println(contactListFromFile.get(i));
+       }
+   }
+   
+   public void addContact() throws IOException {
+	   System.out.println("What is your Contacts name?");
+       name = scanner.next();
+       System.out.println("What is your Contacts phone number?");
+       phoneNumber = scanner.next();
+
+       Files.write(
+               Paths.get("data", "Contacts.txt"),
+               Collections.singletonList(String.format("%-15s | %-15s%n", name, phoneNumber)),
+               StandardOpenOption.APPEND
+       );
+   }
+   
+   public void searchContact() throws IOException{
+	   System.out.println("Enter the Contact you're looking for:");
+       Path contactPath = Paths.get("data", "Contacts.txt");
+       List<String> contactListFromFile = Files.readAllLines(contactPath);
+
+       String name = scanner.next();
+
+       try {
+           scanner = new Scanner(contactPath).useDelimiter(",");
+
+           while (scanner.hasNext()) {
+               final String lineFromFile = scanner.nextLine();
+               if (lineFromFile.contains(name)) {
+                   System.out.println(lineFromFile);
+                   break;
+               }
+           }
+       } catch (IOException e) {
+           System.out.println(" cannot write to file " + contactPath);
+       }
+   }
+   
+   public void createTempFile() throws IOException {
+       try {
+           String directory = "data";
+           String tempFile = "Temp.txt";
+           Path tempDataFile = Paths.get(directory, tempFile);
+
+           Files.createFile(tempDataFile);
+
+       } catch (IOException e) {
+           throw new RuntimeException(e);
+       }
+
+   }
+
+   public void deleteContact(String ContactName) throws IOException {
+       try {
+
+
+           Path contactPath = Paths.get("data", "Contacts.txt");
+           Path tempPath = Paths.get("data", "Temp.txt");
+           List<String> contactListFromFile = Files.readAllLines(contactPath);
+           for (int i = 0; i < contactListFromFile.size(); i += 1) {
+               if (!contactListFromFile.get(i).contains(ContactName)){
+               Files.write(
+                       tempPath,
+                       Collections.singletonList(String.format(contactListFromFile.get(i))),
+                       StandardOpenOption.APPEND
+               );
+
+           }}
+           Files.delete(contactPath);
+           Files.move(tempPath, tempPath.resolveSibling("Contacts.txt"));
+
+
+       } catch (IOException e) {
+           throw new RuntimeException(e);
+       }
+   }
+
 }
--- src/ContactManager.java
+++ src/ContactManager.java
@@ -1,65 +1,19 @@
-import java.io.*;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.*;
+import java.util.Scanner;
+
 
 
 public class ContactManager {
-
-    static void menu() {
-        System.out.println("1. View contacts.");
-        System.out.println("2. Add a new contact.");
-        System.out.println("3. Search a contact by name.");
-        System.out.println("4. Delete an existing contact.");
-        System.out.println("5. Exit.");
-        System.out.println("Enter an option (1-5) then press Enter:");
-
-
-    }
-
-    static void createTempFile() throws IOException {
-        try {
-            String directory = "data";
-            String tempFile = "Temp.txt";
-            Path tempDataFile = Paths.get(directory, tempFile);
-
-            Files.createFile(tempDataFile);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    static void deleteContact(String ContactName) throws IOException {
-        try {
-
-
-            Path contactPath = Paths.get("data", "Contacts.txt");
-            Path tempPath = Paths.get("data", "Temp.txt");
-            List<String> contactListFromFile = Files.readAllLines(contactPath);
-            for (int i = 0; i < contactListFromFile.size(); i += 1) {
-                if (!contactListFromFile.get(i).contains(ContactName)){
-                Files.write(
-                        tempPath,
-                        Collections.singletonList(String.format(contactListFromFile.get(i))),
-                        StandardOpenOption.APPEND
-                );
-
-            }}
-            Files.delete(contactPath);
-            Files.move(tempPath, tempPath.resolveSibling("Contacts.txt"));
-
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
+	static Contact contact = new Contact();
+	static Scanner scanner = new Scanner(System.in);
+	
+	
     public static void main(String[] args) throws IOException {
-
+    	
+    	
         String directory = "data";
         String filename = "Contacts.txt";
 
@@ -78,77 +32,30 @@ public class ContactManager {
         if (!Files.exists(dataFile)) {
             Files.createFile(dataFile);
         }
-        boolean response = true;
-        while (response) {
-            menu();
-            break;
-        }
-
-        Contact contact = new Contact();
-
-
-        Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.next();
-
-
-
-        switch (userInput) {
-            case "1" -> {
-                Path contactPath = Paths.get("data", "Contacts.txt");
-                List<String> contactListFromFile = Files.readAllLines(contactPath);
-                for (int i = 0; i < contactListFromFile.size(); i += 1) {
-
-                    System.out.println(contactListFromFile.get(i));
-                }
-                List<String> lines = Files.readAllLines(Paths.get("data", "Contacts.txt"));
-                menu();
-            }
-
-            case "2" -> {
-                System.out.println("What is your Contacts name?");
-                contact.name = scanner.next();
-                System.out.println("What is your Contacts phone number?");
-                contact.phoneNumber = scanner.next();
-
-                Files.write(
-                        Paths.get("data", "Contacts.txt"),
-                        Collections.singletonList(String.format("%-15s | %-15s%n", contact.name, contact.phoneNumber)),
-                        StandardOpenOption.APPEND
-                );
-            }
-            case "3" -> {
-                System.out.println("Enter the Contact you're looking for:");
-                Path contactPath = Paths.get("data", "Contacts.txt");
-                List<String> contactListFromFile = Files.readAllLines(contactPath);
-
-                String name = scanner.next();
-
-                try {
-                    scanner = new Scanner(contactPath).useDelimiter(",");
-
-                    while (scanner.hasNext()) {
-                        final String lineFromFile = scanner.nextLine();
-                        if (lineFromFile.contains(name)) {
-                            System.out.println(lineFromFile);
-                            break;
-                        }
-                    }
-                } catch (IOException e) {
-                    System.out.println(" cannot write to file " + contactPath);
-                }
-            }
-            case "4" -> {
-                createTempFile();
-                System.out.println("Who would you like to delete from your contacts?");
-                String deletedContact = scanner.next();
-                deleteContact(deletedContact);
-                System.out.println("Do you wish to continue? y/n \n");
-                String confirm = scanner.next();
-            }
-            case "5" -> {
-                System.out.println("Thank you and have a great day!");
-                System.exit(0);
-            }
-        }
+      
+       
+        String userInput = "0";
+        	
+       
+	do {
+    	   contact.menu();
+    	    userInput = scanner.next();
+    	   if(Integer.parseInt(userInput) == 1) {
+    		 contact.showContacts();  
+    	   } else if (Integer.parseInt(userInput) == 2) {
+    		 contact.addContact();
+    	   }else if (Integer.parseInt(userInput) == 3) {
+    		 contact.searchContact();
+    	   }else if (Integer.parseInt(userInput) == 4) {
+    		 contact.createTempFile();
+             System.out.println("Who would you like to delete from your contacts?");
+             String deletedContact = scanner.next();
+             contact.deleteContact(deletedContact);
+    	   }
+       } 
+	
+		while (Integer.parseInt(userInput) != 5);
+        
+      
     }
 }
